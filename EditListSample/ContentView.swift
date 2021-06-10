f24349d8ad7f0e0a1c32f60ed232169b47fb9989
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-10 15:57:25 +0900

    feat: Simple List Edit

--- EditListSample/ContentView.swift
+++ EditListSample/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var fruits = ["apple", "orange", "banana", "pineapple", "strawberry", "mango", "melon"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            List {
+                ForEach.init(fruits, id: \.self) { fruit in
+                    Text(fruit)
+                }.onDelete(perform: { indexSet in
+                    debugPrint(indexSet)
+                    self.fruits.remove(atOffsets: indexSet)
+                }).onMove(perform: { indices, newOffset in
+                    debugPrint(indices)
+                    debugPrint(newOffset)
+                    self.fruits.move(fromOffsets: indices, toOffset: newOffset)
+                })
+            }
+            .navigationBarTitle("Edit List", displayMode: .inline)
+            .navigationBarItems(trailing: EditButton())
+            
+        }
     }
 }
 
